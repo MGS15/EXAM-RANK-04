f41fe7739b75c5c08bdc816e9c448522a44227ad
Soufiane Elkhamlichi <42505085+MGS15@users.noreply.github.com>
2023-05-25 21:56:31 +0100

    update

--- microshell.c
+++ microshell.c
@@ -3,7 +3,7 @@
 #include <string.h>
 #include <sys/wait.h>
 
-int	ft_err(char *e1, char *e2)
+int	err(char *e1, char *e2)
 {
 	while (*e1)
 		write(2, e1++, 1);
@@ -16,39 +16,41 @@ int	ft_err(char *e1, char *e2)
 int	ft_cd(char **cmd, int i)
 {
 	if (i != 2)
-		return (ft_err("error: cd: bad arguments", NULL));
+		return(err("error: cd: bad arguments", NULL));
 	if (chdir(cmd[1]))
-		return (ft_err("error: cd: cannot change directory to ", cmd[1]));
+		return (err("error: cd: cannot change directory to ", cmd[1]));
 	return (0);
 }
 
-int	ft_ex(char **cmd, int i, char **env, int tfd)
+int	ft_executer(char **cmd, int i, char **env, int tfd)
 {
 	cmd[i] = NULL;
 	if (dup2(tfd, 0) == -1)
-		return (ft_err("error: fatal", NULL));
+		return (err("error: fatal", NULL));
 	close(tfd);
 	execve(cmd[0], cmd, env);
-	return (ft_err("error: cannot execute ", cmd[0]));
+	return (err("error: cannot execute ", cmd[0]));
 }
-#include<stdio.h>
+
 int	main(int c, char **v)
 {
+	int	i;
+	int	pid;
+	int	tfd;
+	int	fd[2];
 	extern char	**environ;
-	int			i;
-	int			tfd;
-	int			pid;
-	int			fd[2];
-
+	
 	if (c < 2)
 		return (1);
 	i = 0;
 	tfd = dup(0);
+	if (tfd == -1)
+		return (err("error: fatal", NULL));
 	while (v[i] && v[i + 1])
 	{
-		v = v + i + 1;
+		v = v + 1 + i;
 		i = 0;
-		while (v[i] && strcmp(v[i], ";") && strcmp(v[i], "|"))
+		while (v[i] && strcmp(v[i], "|") && strcmp(v[i], ";"))
 			i++;
 		if (!strcmp(v[0], "cd"))
 			ft_cd(v, i);
@@ -56,10 +58,10 @@ int	main(int c, char **v)
 		{
 			pid = fork();
 			if (pid == -1)
-				return (ft_err("error: fatal", NULL));
+				return (err("error: fatal", NULL));
 			else if (!pid)
 			{
-				if (ft_ex(v, i, environ, tfd))
+				if (ft_executer(v, i, environ, tfd))
 					return (1);
 			}
 			else
@@ -68,21 +70,23 @@ int	main(int c, char **v)
 				while (waitpid(-1, NULL, 0) != -1);
 			}
 			tfd = dup(0);
+			if (tfd == -1)
+				return (err("error: fatal", NULL));
 		}
 		else if (i && !strcmp(v[i], "|"))
 		{
 			if (pipe(fd) == -1)
-				return (ft_err("error: fatal", NULL));
+				return (err("error: fatal", NULL));
 			pid = fork();
 			if (pid == -1)
-				return (ft_err("error: fatal", NULL));
+				return (err("error: fatal", NULL));
 			else if (!pid)
 			{
 				if (dup2(fd[1], 1) == -1)
-					return (ft_err("error: fatal", NULL));
+					return (err("error: fatal", NULL));
 				close(fd[0]);
 				close(fd[1]);
-				if (ft_ex(v, i, environ, tfd))
+				if (ft_executer(v, i, environ, tfd))
 					return (1);
 			}
 			else
